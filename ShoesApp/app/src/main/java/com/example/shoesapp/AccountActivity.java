7fd204999983aa88fc9bbcc4655385abb32eb0bf
ngocthaotn <55819513+ngocthaotn@users.noreply.github.com>
2020-05-23 10:52:01 +0700

    chay duoc bottom navigation

---
+++ ShoesApp/app/src/main/java/com/example/shoesapp/AccountActivity.java
@@ -0,0 +1,19 @@
+package com.example.shoesapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AccountActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_account, container, false) ;
+
+    }
+}
--- ShoesApp/app/src/main/java/com/example/shoesapp/MainActivity.java
+++ ShoesApp/app/src/main/java/com/example/shoesapp/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+
+import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -27,102 +30,96 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle t;
     private NavigationView nv;
     private BottomNavigationView mainNav;
-    private ScrollView scrollMainHome;
     private FrameLayout mainFrame;
-
-    private OrderFragment orderFragment;
-    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_infoproducts);
 
-        dl = (DrawerLayout)findViewById(R.id.activity_main);
-        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+//        dl = (DrawerLayout)findViewById(R.id.activity_main);
+//        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+//
+//        dl.addDrawerListener(t);
+//        t.syncState();
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        nv = (NavigationView)findViewById(R.id.nv);
+//        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                int id = item.getItemId();
+//                switch(id)
+//                {
+//                    case R.id.trangchu:
+//                        Toast.makeText(MainActivity.this, "Trang chủ",Toast.LENGTH_SHORT).show();break;
+//                    case R.id.taikhoan:
+//                        Toast.makeText(MainActivity.this, "Tài khoản",Toast.LENGTH_SHORT).show();break;
+//                    case R.id.giohang:
+//                        Toast.makeText(MainActivity.this, "Giỏ hàng", Toast.LENGTH_SHORT).show();break;
+//                    case R.id.dangxuat:
+//                        Toast.makeText(MainActivity.this, "Đăng xuất", Toast.LENGTH_SHORT).show();break;
+//                    default:
+//                        return true;
+//                }
+//                return true;
+//            }
+//        });
 
-        dl.addDrawerListener(t);
-        t.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        nv = (NavigationView)findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch(id)
-                {
-                    case R.id.trangchu:
-                        Toast.makeText(MainActivity.this, "Trang chủ",Toast.LENGTH_SHORT).show();break;
-                    case R.id.taikhoan:
-                        Toast.makeText(MainActivity.this, "Tài khoản",Toast.LENGTH_SHORT).show();break;
-                    case R.id.giohang:
-                        Toast.makeText(MainActivity.this, "Giỏ hàng", Toast.LENGTH_SHORT).show();break;
-                    case R.id.dangxuat:
-                        Toast.makeText(MainActivity.this, "Đăng xuất", Toast.LENGTH_SHORT).show();break;
-                    default:
-                        return true;
-                }
-                return true;
-            }
-        });
-
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager_infoproducts);
         ImageAdapter adapter = new ImageAdapter(this);
         mViewPager.setAdapter(adapter);
 
-        //load video
-        mvideoView = (VideoView) findViewById(R.id.videoView);
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.gas );
-        try {
-            mvideoView.setVideoURI(uri);
-        } catch (NullPointerException techmaster1)
-        {
-            System.out.println("Loi khong load duoc video" + techmaster1);
-        }
-        mvideoView.start();
+//        //load video
+//        mvideoView = (VideoView) findViewById(R.id.videoView);
+//        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.gas );
+//        try {
+//            mvideoView.setVideoURI(uri);
+//        } catch (NullPointerException techmaster1)
+//        {
+//            System.out.println("Loi khong load duoc video" + techmaster1);
+//        }
+//        mvideoView.start();
 
         //bottom navigation
 
-        accountFragment = new AccountFragment();
-        orderFragment = new OrderFragment();
-
-        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
-        mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
-        mainNav.clearFocus();
-
-        mainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-//                    case R.id.nav_home:
-//                        return true;
-                    case R.id.nav_cart:
-                        setFragment(orderFragment);
-                        return true;
-                    case R.id.nav_account:
-                        setFragment(accountFragment);
-                        return true;
-
-                    default:
-                        return false;
-                }
-            }
-        });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(t.onOptionsItemSelected(item))
-            return true;
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void setFragment (Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
+//        mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+//        mainNav.setOnNavigationItemSelectedListener(navListener);
+//        mainNav.clearFocus();
+//
+////        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeActivity()).commit();
+//
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        if(t.onOptionsItemSelected(item))
+//            return true;
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//            Fragment selectedFragment = null;
+//
+//            switch (menuItem.getItemId()){
+//                case R.id.nav_home:
+//                    selectedFragment = new HomeActivity();
+//                    break;
+//                case R.id.nav_cart:
+//                    selectedFragment = new OrderActivity();
+//                    break;
+//                case R.id.nav_account:
+//                    selectedFragment = new AccountActivity();
+//                    break;
+//            }
+//            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, selectedFragment).commit();
+//
+//            return false;
+//        }
+//    };
     }
 }
